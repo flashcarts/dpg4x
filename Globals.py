197a851d364c84d5720bc0b45ba67a29e3f6bfd0
Félix Medrano <xukosky@users.sourceforge.net>
2010-05-30 17:04:37 +0000

    tags folder fixed (I hope)

---
+++ Globals.py
@@ -0,0 +1,241 @@
+# -*- coding: utf-8 -*-
+
+#----------------------------------------------------------------------------
+# Name:         Globals.py
+# Purpose:      Source file with global variables and functions.
+#
+# Author:       Félix Medrano Sanz
+#
+# Created:      
+# RCS-ID:       $Id: Globals.py $
+# Copyright:    (c) 2009 Félix Medrano Sanz
+# Licence:      GPL v3
+#----------------------------------------------------------------------------
+
+import ConfigurationManager
+
+import os
+import sys
+import string
+import tempfile
+import shutil
+
+###############
+## VARIABLES ##
+###############
+
+# Configuration files
+FILECONFIG = [os.path.expanduser("~/.config/dpg4x/config.ini")]
+# User config file
+USERFILECONFIG = FILECONFIG[0]
+
+# Temporary files
+TMP_AUDIO = None
+TMP_VIDEO = None
+TMP_HEADER = None
+TMP_GOP = None
+TMP_STAT = None
+TMP_THUMB = None
+TMP_SHOT = None
+TMP_DIVX2PASS = None
+	
+# Option panels
+mainPanel = None
+filesPanel = None
+videoPanel = None
+audioPanel = None
+subtitlesPanel = None
+otherPanel = None
+
+# General options
+dpg_version = 4
+dpg_quality = 'normal'
+
+# Video options
+video_keepaspect = False
+video_width = 256
+video_height = 192
+video_bitrate = 288
+video_fps = 15
+video_autofps = True
+video_pixel = 3
+# 0 RGB15
+# 1 RGB18
+# 2 RGB21
+# 3 RGB24 
+
+# Audio options
+audio_codec = 'mp2'
+audio_track = 1
+audio_autotrack = True
+audio_bitrate = 128
+audio_frequency = 32000
+audio_normalize = False
+audio_mono = False
+
+# Subtitle options
+subtitles_source = 'auto'
+subtitles_track = 1
+subtitles_file = ''
+subtitles_font = 'Sans'
+subtitles_encoding = sys.getfilesystemencoding()
+
+# Other output
+other_output = ''
+other_temporary = '/tmp'
+other_thumbnail = ''
+
+
+###############
+## FUNCTIONS ##
+###############
+
+def debug(message):
+    "Shows a message in the error output"
+    sys.stderr.write((message+"\n").encode(
+        sys.getfilesystemencoding(),'replace'))
+
+def Encode(text):
+    "Encode text to be system-encoding compatible"
+    return text.encode(sys.getfilesystemencoding(),'replace')
+
+def Decode(text):
+    "Decode text to be system-encoding compatible"
+    return text.decode(sys.getfilesystemencoding(),'replace')
+        
+def CreateFolder(dir):
+    "Create a folder with full path"
+    if not os.path.isdir(dir):
+        chunks = dir.split("/")
+        subchunk = "/"
+        for chunk in chunks:
+            subchunk = os.path.join(subchunk, chunk)
+            if not os.path.isdir(subchunk):
+                #os.mkdir(subchunk, stat.S_IRWXU) UMASK?
+                os.mkdir(subchunk)
+
+# Stolen from Noah Spurrier's pexpect. Thank you Noah!
+# Pexpect it's a great library and I use it often. 
+# But now, I just need this function, so I'll try to save on dependencies...
+def which (filename):
+
+    """This takes a given filename; tries to find it in the environment path;
+    then checks if it is executable. This returns the full path to the filename
+    if found and executable. Otherwise this returns None."""
+
+    # Special case where filename already contains a path.
+    if os.path.dirname(filename) != '':
+        if os.access (filename, os.X_OK):
+            return filename
+
+    if not os.environ.has_key('PATH') or os.environ['PATH'] == '':
+        p = os.defpath
+    else:
+        p = os.environ['PATH']
+
+    # Oddly enough this was the one line that made Pexpect
+    # incompatible with Python 1.5.2.
+    #pathlist = p.split (os.pathsep)
+    pathlist = string.split (p, os.pathsep)
+
+    for path in pathlist:
+        f = os.path.join(path, filename)
+        if os.access(f, os.X_OK):
+            return f
+    return None
+
+def concat(out,*files):
+    "Concatenate a list of files"
+    outfile = open(out,'w')
+    for name in files:
+        outfile.write( open(name).read() )
+    outfile.close()
+    
+def createTemporary():
+    "Create the temporary files needed to encode"
+    
+    # Folder for the temporary files
+    global other_temporary
+    tmpDir = other_temporary
+    
+    # Audio temporary file
+    global TMP_AUDIO
+    fd,TMP_AUDIO = tempfile.mkstemp(dir=tmpDir)
+    os.close(fd)
+	# Video temporary file
+    global TMP_VIDEO
+    fd,TMP_VIDEO = tempfile.mkstemp(dir=tmpDir)
+    os.close(fd)
+	# Header temporary file
+    global TMP_HEADER
+    fd,TMP_HEADER = tempfile.mkstemp(dir=tmpDir)
+    os.close(fd)
+	# GOP offsets temporaries
+	# Only needed with dpg_version >= 2
+    global dpg_version
+    global TMP_GOP
+    global TMP_STAT
+    if dpg_version >= 2:
+        fd,TMP_GOP = tempfile.mkstemp(dir=tmpDir)
+        os.close(fd)
+    # TMP_STAT always needed for frames calculation
+    fd,TMP_STAT = tempfile.mkstemp(dir=tmpDir)
+    os.close(fd)
+    # Thumbnail temporary files
+    global other_thumbnail
+    global TMP_THUMB
+    global TMP_SHOT
+    fd,TMP_THUMB = tempfile.mkstemp(dir=tmpDir)
+    os.close(fd)
+    # Only needed on autogenerate
+    if not other_thumbnail:
+        TMP_SHOT = tempfile.mkdtemp(dir=tmpDir)
+    # Divx 2-pass log file
+    # Only needes for very high quality encode
+    global TMP_DIVX2PASS
+    if dpg_quality == 'doublepass':
+        TMP_DIVX2PASS = tempfile.mkdtemp(dir=tmpDir)
+        
+def clearTemporary():
+    "Delete the temporary files"
+    try:
+        # Audio temporary file
+        global TMP_AUDIO
+        if TMP_AUDIO and os.path.isfile(TMP_AUDIO):
+            os.remove(TMP_AUDIO)
+        # Video temporary file
+        global TMP_VIDEO
+        if TMP_VIDEO and os.path.isfile(TMP_VIDEO):
+            os.remove(TMP_VIDEO)
+        # Header temporary file
+        global TMP_HEADER
+        if TMP_HEADER and os.path.isfile(TMP_HEADER):
+            os.remove(TMP_HEADER)
+        # GOP offsets temporaries
+        global TMP_GOP
+        global TMP_STAT
+        if TMP_GOP and os.path.isfile(TMP_GOP):
+            os.remove(TMP_GOP)
+        if TMP_STAT and os.path.isfile(TMP_STAT):
+            os.remove(TMP_STAT)
+        # Thumbnail temporary files
+        global TMP_THUMB
+        global TMP_SHOT
+        if TMP_THUMB and os.path.isfile(TMP_THUMB):
+            os.remove(TMP_THUMB)
+        # Note that TMP_SHOT is a folder, not a file
+        if TMP_SHOT and os.path.isdir(TMP_SHOT):
+            shutil.rmtree(TMP_SHOT, ignore_errors = True)
+        # Same with TMP_DIVX2PASS
+        global TMP_DIVX2PASS
+        if TMP_DIVX2PASS and os.path.isdir(TMP_DIVX2PASS):
+            shutil.rmtree(TMP_DIVX2PASS, ignore_errors = True)
+            
+    # Warn if there is a problem when deleting files
+    except Exception, e:
+        debug(_(u'WARNING: Temporary files were not properly deleted:') + '' \
+            u' ' + unicode(e))
+            
+# Load the configuration file
+ConfigurationManager.loadConfiguration()
+
---
+++ SubtitlesPanel.py
@@ -0,0 +1,252 @@
+#Boa:FramePanel:Panel1
+# -*- coding: utf-8 -*-
+
+#----------------------------------------------------------------------------
+# Name:         SubtitlesPanel.py
+# Purpose:      Panel with subtitle options.
+#
+# Author:       Félix Medrano Sanz
+#
+# Created:      
+# RCS-ID:       $Id: SubtitlesPanel.py $
+# Copyright:    (c) 2009 Félix Medrano Sanz
+# Licence:      GPL v3
+#----------------------------------------------------------------------------
+
+import Globals
+import TreeCtrlComboPopup
+import CustomFontSelector
+
+import wx
+import subprocess
+import os
+
+[wxID_PANEL1, wxID_PANEL1BUTTON1, wxID_PANEL1BUTTON2, 
+ wxID_PANEL1TREECTRLCOMBO1, wxID_PANEL1SPINCTRL1, wxID_PANEL1STATICTEXT1, 
+ wxID_PANEL1STATICTEXT2, wxID_PANEL1STATICTEXT3, wxID_PANEL1STATICTEXT4, 
+ wxID_PANEL1TEXTCTRL1, wxID_PANEL1TEXTCTRL2, wxID_PANEL1STATICTEXT5,
+ wxID_PANEL1CHOICE2
+] = [wx.NewId() for _init_ctrls in range(13)]
+
+class SubtitlesPanel(wx.Panel):
+    def _init_coll_gridBagSizer1_Items(self, parent):
+        # generated method, don't edit
+
+        parent.AddSpacer(wx.Size(30, 20), (0, 0), border=0, flag=0, span=(1, 1))
+        parent.AddSpacer(wx.Size(100, 8), (0, 1), border=0, flag=0, span=(1, 2))
+        #parent.AddSpacer(wx.Size(200, 8), (0, 3), border=0, flag=0, span=(1, 3))
+        parent.AddWindow(self.staticText5, (1, 1), border=0, 
+              flag=wx.ALIGN_RIGHT | wx.ALIGN_CENTER_VERTICAL, span=(1, 2))
+        parent.AddWindow(self.choice2, (1, 3), border=0, flag=0, span=(1, 4))
+        parent.AddWindow(self.staticText1, (3, 1), border=0, 
+              flag=wx.ALIGN_RIGHT | wx.ALIGN_CENTER_VERTICAL, span=(1, 2))
+        parent.AddWindow(self.spinCtrl1, (3, 3), border=0, flag=0, span=(1, 1))
+        parent.AddWindow(self.staticText2, (5, 1), border=0, 
+              flag=wx.ALIGN_RIGHT | wx.ALIGN_CENTER_VERTICAL, span=(1,2))
+        parent.AddWindow(self.textCtrl1, (5, 3), border=0, flag=wx.EXPAND,
+              span=(1, 3))
+        parent.AddWindow(self.button1, (5, 6), border=0, flag=0, span=(1, 1))
+        parent.AddWindow(self.staticText3, (8, 1), border=0, 
+              flag=wx.ALIGN_RIGHT | wx.ALIGN_CENTER_VERTICAL, span=(1,2))
+        parent.AddWindow(self.textCtrl2, (8, 3), border=0, flag=wx.EXPAND,
+              span=(1, 3))
+        parent.AddWindow(self.button2, (8, 6), border=0, flag=0, span=(1, 1))
+        parent.AddWindow(self.staticText4, (10, 1), border=0, 
+              flag=wx.ALIGN_RIGHT | wx.ALIGN_CENTER_VERTICAL, span=(1, 2))
+        parent.AddWindow(self.choice1, (10, 3), border=0, flag=0, span=(1, 3))
+
+    def _init_sizers(self):
+        # generated method, don't edit
+        self.gridBagSizer1 = wx.GridBagSizer(hgap=0, vgap=0)
+
+        self._init_coll_gridBagSizer1_Items(self.gridBagSizer1)
+
+        self.SetSizer(self.gridBagSizer1)
+
+    def _init_ctrls(self, prnt):
+        # generated method, don't edit
+        wx.Panel.__init__(self, id=wxID_PANEL1, name='', parent=prnt,
+              style=wx.TAB_TRAVERSAL)
+        self.SetClientSize(wx.Size(565, 300))
+              
+        self.staticText5 = wx.StaticText(id=wxID_PANEL1STATICTEXT5,
+              label=_(u'Subtitles Source')+' ', name='staticText5', parent=self, 
+              style=0)
+
+        self.choice2 = wx.Choice(choices=[], id=wxID_PANEL1CHOICE2,
+              name='choice1', parent=self, style=0)
+
+        self.staticText1 = wx.StaticText(id=wxID_PANEL1STATICTEXT1,
+              label=_(u'Subtitles Track')+' ', name='staticText1', 
+              parent=self, style=0)
+
+        self.spinCtrl1 = wx.SpinCtrl(id=wxID_PANEL1SPINCTRL1, 
+              initial=Globals.subtitles_track,
+              max=14, min=0, name='spinCtrl1', parent=self, 
+              style=wx.SP_ARROW_KEYS)
+
+        self.staticText2 = wx.StaticText(id=wxID_PANEL1STATICTEXT2,
+              label=_(u'Subtitles File')+' ', name='staticText2', 
+              parent=self, style=0)
+
+        self.textCtrl1 = wx.TextCtrl(id=wxID_PANEL1TEXTCTRL1, name='textCtrl1',
+              parent=self, style=0, value=Globals.subtitles_file)
+
+        self.button1 = wx.Button(id=wxID_PANEL1BUTTON1, label=_(u'Examine'),
+              name='button1', parent=self, style=0)
+
+        self.staticText3 = wx.StaticText(id=wxID_PANEL1STATICTEXT3,
+              label=_(u'Font')+' ', name='staticText3', parent=self, style=0)
+
+        self.textCtrl2 = wx.TextCtrl(id=wxID_PANEL1TEXTCTRL2, name='textCtrl2',
+              parent=self, style=wx.TE_READONLY, value=Globals.subtitles_font)
+
+        self.button2 = wx.Button(id=wxID_PANEL1BUTTON2, label=_(u'Examine'),
+              name='button2', parent=self, style=0)
+
+        self.staticText4 = wx.StaticText(id=wxID_PANEL1STATICTEXT4,
+              label=_(u'Encoding')+' ', name='staticText4', parent=self, style=0)
+
+        # Custom TreeCtrlComboPopup
+        self.choice1 = wx.combo.ComboCtrl(self, size=(280,-1), 
+            style=wx.CB_READONLY, value=Globals.subtitles_encoding)
+        self.treeCtrlCombo = TreeCtrlComboPopup.TreeCtrlComboPopup()
+        self.choice1.SetPopupControl(self.treeCtrlCombo)
+
+        self._init_sizers()
+
+    def __init__(self, parent):
+        self._init_ctrls(parent)
+        
+        # Init the subtitles source choice
+        self.choice2.Append(_(u'Auto'),'auto')
+        self.choice2.Append(_(u'Get track from input video file'),'sid')
+        self.choice2.Append(_(u'Use subtitles file'),'file')
+        self.choice2.Append(_(u'Disable subtitles'),'disable')
+        if Globals.subtitles_source == 'auto':
+            self.choice2.Select(0)
+        elif Globals.subtitles_source == 'sid':
+            self.choice2.Select(1)
+        elif Globals.subtitles_source == 'file':
+            self.choice2.Select(2)
+        elif Globals.subtitles_source == 'disable':
+            self.choice2.Select(3)
+        self.changeSubsSource(None)
+        
+        # Init encoding choice
+        default = Globals.subtitles_encoding
+        if Globals.which('iconv'):
+            iconvOut = subprocess.Popen(
+                ['iconv', '-l'], stdout=subprocess.PIPE).communicate()[0]
+            iconvOut = iconvOut.replace('/','')
+            encodings = iconvOut.split('\n')
+            encodings.sort()
+            # Load the encodings in the tree
+            currentLetter = chr(0)
+            currentNode = None               
+            for encode in encodings:
+                # Careful with empty strings
+                if len(encode) > 1:
+                    firstLetter = encode[0].upper()
+                    # Detect numbers
+                    if firstLetter >= '0' and firstLetter <= '9':
+                        firstLetter = '#'
+                    # If the firs letter has changed create a new node
+                    if firstLetter != currentLetter:
+                        currentLetter = firstLetter
+                        currentNode = self.treeCtrlCombo.AddItem(currentLetter)
+                    # Add the encoding to the tree
+                    sub = self.treeCtrlCombo.AddItem(encode, parent=currentNode)
+                    # Check if it's de default and select it
+                    if encode == default:
+                        self.treeCtrlCombo.GetControl().SelectItem(sub)
+                    
+        # If iconv is not available, disable encoding
+        else:
+            self.choice1.SetValue('')
+            self.choice1.Enable(False)
+            Globals.debug(_(u'WARNING: iconv not found. Subtitles encoding ' \
+                u'option disabled.'))
+        
+        # Events
+        wx.EVT_BUTTON(self.button1, wxID_PANEL1BUTTON1, self.selectSubsFile)
+        wx.EVT_BUTTON(self.button2, wxID_PANEL1BUTTON2, self.selectSubsFont)
+        wx.EVT_CHOICE(self.choice2, wxID_PANEL1CHOICE2, self.changeSubsSource)
+        
+    def changeSubsSource(self, event):
+        "Change the source for the subtitles"
+        # If None event we called it
+        if (event is not None):
+            event.StopPropagation()
+        #source = event.GetClientData()
+        source = self.choice2.GetClientData(self.choice2.GetSelection())
+        # Start enabling all the controls
+        self.spinCtrl1.Enable(True)
+        self.textCtrl1.Enable(True)
+        self.button1.Enable(True)
+        self.textCtrl2.Enable(True)
+        self.button2.Enable(True)
+        self.choice1.Enable(True)
+        # If auto, disable the sid and the subs file
+        if source == 'auto':
+            self.spinCtrl1.Enable(False)
+            self.textCtrl1.Enable(False)
+            self.button1.Enable(False)
+        # If sid, disable the subs file
+        elif source == 'sid':
+            self.textCtrl1.Enable(False)
+            self.button1.Enable(False)
+        # If file, disable the sid
+        elif source == 'file':
+            self.spinCtrl1.Enable(False)
+        # Else disable all the controls
+        else:
+            self.spinCtrl1.Enable(False)
+            self.textCtrl1.Enable(False)
+            self.button1.Enable(False)
+            self.textCtrl2.Enable(False)
+            self.button2.Enable(False)
+            self.choice1.Enable(False)
+        
+    def selectSubsFile(self, event):
+        "Dialog to select a subtitles file"
+        # If None event we called it
+        if (event is not None):
+            event.StopPropagation()
+        dialog = wx.FileDialog(self, _(u'Select a subtitles file'), style=wx.OPEN, 
+            defaultDir=os.path.dirname(self.textCtrl1.GetValue()))
+        if dialog.ShowModal() == wx.ID_OK:
+            self.textCtrl1.SetValue(dialog.GetPath())
+        dialog.Destroy()
+        
+    def selectSubsFont(self, event):
+        "Dialog to select a subtitles font"
+        # If None event we called it
+        if (event is not None):
+            event.StopPropagation()
+        dialog = CustomFontSelector.CustomFontSelector(
+            self, _(u'Select the subtitles font'), 
+            self.textCtrl2.GetValue())
+        if dialog.ShowModal() == wx.ID_OK:
+            self.textCtrl2.SetValue(dialog.GetData())
+        dialog.Destroy()
+        
+    def loadOptions(self):
+        "Load the subtitle options as global variables"
+        Globals.subtitles_source = self.choice2.GetClientData(
+            self.choice2.GetSelection())
+        # If subtitles track selected
+        if Globals.subtitles_source == 'sid':
+            Globals.subtitles_track = self.spinCtrl1.GetValue()
+        # If subtitles file selected
+        elif Globals.subtitles_source == 'file':
+            Globals.subtitles_file = self.textCtrl1.GetValue()
+            # Check the subtitles file can be readed
+            if not (os.path.isfile(Globals.subtitles_file) and os.access(
+                Globals.subtitles_file, os.R_OK)):
+                raise Exception(_(u'The subtitles file can not be readed.'))
+        # Get also fonts and encoding
+        Globals.subtitles_font = self.textCtrl2.GetValue()
+        Globals.subtitles_encoding = self.choice1.GetValue()
+
+
